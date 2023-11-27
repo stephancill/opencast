74fa8af5ddb622b12132f260edadb3869dffeb68
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-11-27 23:56:23 +0200

    fix: builds

--- src/lib/embeds.ts
+++ src/lib/embeds.ts
@@ -1,12 +1,31 @@
+import { UserDataType } from '@farcaster/hub-nodejs';
 import { Embed as ModEmbed } from '@mod-protocol/core';
-import { Tweet } from './types/tweet';
+import { isFarcasterUrlEmbed } from '@mod-protocol/farcaster';
+import { casts } from '@prisma/client';
+import getMetaData from 'metadata-scraper';
+import { LRU } from './lru-cache';
+import { prisma } from './prisma';
+import { ExternalEmbed, Tweet } from './types/tweet';
 import { User, UsersMapType } from './types/user';
 import { resolveUserFromFid } from './user/resolve-user';
 import { isValidImageExtension } from './validation';
-import { UserDataType } from '@farcaster/hub-nodejs';
-import { casts } from '@prisma/client';
-import { isFarcasterUrlEmbed } from '@mod-protocol/farcaster';
-import { prisma } from './prisma';
+
+const KNOWN_HOSTS_MAP: {
+  [key: string]: { urlBuilder?: (url: string) => string; userAgent?: string };
+} = {
+  'twitter.com': {
+    urlBuilder: (url: string) => url.replace('twitter.com', 'fxtwitter.com'),
+    userAgent: 'bot'
+  },
+  'x.com': {
+    urlBuilder: (url: string) => url.replace('x.com', 'fxtwitter.com'),
+    userAgent: 'bot'
+  },
+  'arxiv.org': {
+    userAgent:
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+  }
+};
 
 async function getEmbedsForTweetIds(ids: string[]) {
   const request = await fetch(
@@ -122,4 +141,60 @@ export async function populateEmbedsForTweets(
   );
 
   return merged;
+}
+
+// TODO: Refactor to use opengraph api
+export async function populateEmbed(
+  embed: ExternalEmbed
+): Promise<ExternalEmbed | null> {
+  let host = '';
+  try {
+    host = new URL(embed.url).host;
+  } catch (e) {
+    console.error(`Error parsing URL ${embed.url}`);
+    return null;
+  }
+
+  const url = KNOWN_HOSTS_MAP[host]?.urlBuilder?.(embed.url) || embed.url;
+  const cached = LRU.get(url);
+  if (cached !== undefined) {
+    return cached;
+  }
+
+  let result: ExternalEmbed | null = null;
+
+  try {
+    const userAgent = KNOWN_HOSTS_MAP[host]?.userAgent || undefined;
+
+    const metadata = await getMetaData(url, {
+      maxRedirects: 1,
+      timeout: 1000,
+      ua:
+        userAgent ||
+        'Mozilla/5.0 (compatible; TelegramBot/1.0; +https://core.telegram.org/bots/webhooks)'
+    });
+    const { title, description, icon, image } = metadata;
+
+    if (title || description) {
+      const populatedEmbed: ExternalEmbed = {
+        url: embed.url,
+        title: title,
+        text: description,
+        icon,
+        image
+      };
+      LRU.set(url, populatedEmbed);
+      result = populatedEmbed;
+    }
+  } catch (e) {
+    console.log(`Error fetching embed for ${url}`);
+    // console.error(e);
+  }
+
+  // If we get an error, cache the error for an hour
+  LRU.set(url, result, { ttl: result === null ? 1000 * 60 * 60 : undefined });
+
+  // console.log(`Finished populating embed for ${embed.url}`, result);
+
+  return result;
 }
